87de6cf9cb5b72700525cc5e91766c46fceb0153
Gdsboom <rru.12@mail.ru>
2025-02-02 15:19:16 +0300

    first commit

---
+++ duckchat_tor/DuckChat_Tor/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.1'
+
+from .DuckChat_Tor import DuckChat_Tor
+
+# Можно также определить версию библиотеки
---
+++ duckchat_tor/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DuckChat_Tor',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'selenium>=4.27.1'  # Укажите минимальную версию Selenium
+    ],
+    author='Ваше имя',
+    author_email='None',
+    description='Краткое описание вашей библиотеки',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='None',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+)
