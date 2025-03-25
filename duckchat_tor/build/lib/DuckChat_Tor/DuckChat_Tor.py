74baf1ed3794acde2f7aef8187a7c4bc3937ac56
Gdsboom <rru.12@mail.ru>
2025-03-25 17:54:13 +0300

    Исправлен баг с не вводом в user-prompt

--- duckchat_tor/build/lib/DuckChat_Tor/DuckChat_Tor.py
+++ duckchat_tor/build/lib/DuckChat_Tor/DuckChat_Tor.py
@@ -75,18 +75,6 @@ class DuckChat_Tor:
         #self.firefox_options.add_argument("--profile")
         #self.firefox_options.add_argument(f"{tor_browser_path}/TorBrowser/Data/Browser/profile.default")
         self.driver = webdriver.Firefox(service=FirefoxService(service), options=self.firefox_options)
-        """
-        try:
-            # Ждём, пока не исчезнут все элементы на странице
-            WebDriverWait(self.driver, 20).until(
-                lambda d: len(d.find_elements(By.XPATH, "//*")) == 0
-            )
-
-            # print("Страница загрузилась полностью и является пустой.")
-
-        except Exception as e:
-            pass
-        """
         while True:
             # Ожидание появления элемента <head>
             try:
@@ -221,33 +209,7 @@ class DuckChat_Tor:
                 except Exception as e:
                     pass
                     # print(f"Ошибка при завершении процесса {proc.info['pid']}: {e}")
-    """
-    def __close_tor_browser(self):
-        for proc in psutil.process_iter(['pid', 'name']):
-            if proc.info['name'] == 'firefox1.exe':  # Убедитесь, что имя процесса соответствует вашему Tor Browser
-                # Завершаем дочерние процессы
-                children = proc.children(recursive=True)
-                for child in children:
-                    child.kill()  # или child.kill() для принудительного завершения
 
-                # Завершаем родительский процесс
-                proc.kill()  # или proc.kill() для принудительного завершения
-                #print(f"Процесс {proc.info['pid']} и его дочерние процессы успешно завершены.")
-        '''
-        for proc in psutil.process_iter(['pid', 'name']):
-            if proc.info['name'] == 'geckodriver.exe':  # Убедитесь, что имя процесса соответствует вашему Tor Browser
-                # Завершаем дочерние процессы
-                try:
-                    children = proc.children(recursive=True)
-                    for child in children:
-                        child.kill()  # или child.kill() для принудительного завершения
-                except:
-                    pass
-                # Завершаем родительский процесс
-                proc.kill()  # или proc.kill() для принудительного завершения
-                #print(f"Процесс {proc.info['pid']} и его дочерние процессы успешно завершены.")
-        '''
-    """
     def wait_page(self):
         while True:
             try:
@@ -311,17 +273,11 @@ class DuckChat_Tor:
 
     def GetPrompt(self, text):
         self.wait_page()
-        self.driver.find_element(By.NAME, "user-prompt").click()
+        #self.driver.find_element(By.NAME, "user-prompt").click()
         bufelement = self.driver.find_element(By.NAME, "user-prompt").send_keys(text)
         #for char in text:
             #bufelement.send_keys(char)
             #time.sleep(0.01)  # Задержка между вводом каждого символа
-        try:
-            pass
-        except Exception as e:
-            print("Ошибка!", e)
-            pass
-
         # print("1!")
         # Локатор кнопки (например, по атрибуту aria-label)
         button_locator = (By.CSS_SELECTOR, 'button[type="submit"]')
@@ -344,13 +300,10 @@ class DuckChat_Tor:
                     break  # Выходим из цикла, если клик успешен
                 except Exception as e:
                     print(f"Не удалось кликнуть по кнопке: {e}")
-            # self.driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
-            # print("4!")
+
 
         wait_time = 5
         start_time = time.time()
-
-
         while True:
                 try:
                     # Явное ожидание, чтобы убедиться, что элемент загружен
@@ -360,51 +313,10 @@ class DuckChat_Tor:
                     p_element = wait.until(EC.presence_of_element_located(
                         (By.XPATH, f"//div[starts-with(@heading, '{self.model}')]//p[not(@class)]")
                     ))
-                    #print("2!")
                     # Извлеките текст из элемента <p>
                     p_text = p_element.text
                     print(p_text)
                     return p_text
-                    """
-                    # Ждем, пока элементы с атрибутом heading появятся на странице
-                    # elements = WebDriverWait(self.driver, 2).until(
-                    #    EC.presence_of_all_elements_located((By.XPATH, '//*[@heading="GPT-4o mini"]'))
-                    # )
-                    # Ждем, пока все div с заголовками загрузятся
-                    # Ожидаем, пока все div элементы загрузятся
-                    elements = WebDriverWait(self.driver, 10).until(
-                        EC.presence_of_all_elements_located((By.TAG_NAME, 'div')))
-                    print(f'Найдено {len(elements)} элементов div.')
-                    for element in elements:
-                        # print("1!")
-                        try:
-                            pass
-                        except:
-                            pass
-                        # Проверяем, содержится ли нужное слово в тексте div
-                        if self.model in element.text and "You are chatting with" not in element.text:
-                            # print(element.text, self.model in element.text)
-                            ps = element.find_elements(By.TAG_NAME, "p")
-                            print( "ps:",len(ps) )
-                            for p in ps:
-                                # print("1!   ", p.text)
-                                if not (self.model in p.text) and p.text != text:
-                                    # print("2!")
-                                    # Извлекаем текст из указанного XPath
-                                    # Получаем путь к текущему скрипту
-                                    current_directory = os.path.dirname(os.path.abspath(__file__))
-
-                                    # Указываем имя файла для скриншота
-                                    screenshot_filename = os.path.join(current_directory, 'screenshot.png')
-
-                                    # Делаем скриншот и сохраняем его
-                                    screenshot = pyautogui.screenshot()
-                                    screenshot.save(screenshot_filename)
-                                    return p.text
-
-                            # print("Ошибка в ожиданнии получения ответа")
-                        # print("Произошла ошибка в поисках ответа нейронки:", e)
-                        """
 
                 except Exception as e:
                     #print("Ошбика в ожидании ответа!")
@@ -456,40 +368,6 @@ class DuckChat_Tor:
             self.new_chat()
 
             return self.GetPrompt(text)
-
-
-    """
-    # Находим все кнопки внутри текущего элемента
-                            buttons = element.find_elements(By.TAG_NAME, "button")
-                            # Проверяем, что количество кнопок равно 2
-                            if len(buttons) == 4:
-                                start_time1 = time.time()
-
-                                while True:
-                                    try:
-                                        # driver.find_element(By.XPATH,"/html/body/div[2]/div[6]/div[4]/div/div[2]/main/div/section[2]/div/div[2]/form/div[1]/div[2]/button").click()
-                                        buff_element = element.find_element(By.CSS_SELECTOR,
-                                                                            'p[class="_7Umba5sjAq6KV8_X26Kg wZ4JdaHxSAhGy1HoNVja yPVtygfW3yi77pB7G6Se"]')
-                                        # asdas = buff_element.text
-                                        # if (buff_element.text == "GPT-4o mini"):
-                                        if (self.model in buff_element.text):
-                                            break
-
-                                        if time.time() - start_time1 > wait_time:
-                                            # print("Время ожидания истекло.")
-                                            break
-
-                                    except Exception as e:
-                                        pass
-                                        # print("Не то: ", e)
-                                    if time.time() - start_time1 > wait_time:
-                                        # print("Время ожидания истекло.")
-                                        break
-    """
-
-
-
-
 
     def new_chat(self):
         buttons = WebDriverWait(self.driver, 10).until(
